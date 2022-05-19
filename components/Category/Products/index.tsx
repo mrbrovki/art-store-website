f1a5efb32a51678606000ea484b41452ac10f339
Mahkamjon Raupov <devbrovki@gmail.com>
2022-05-19 23:58:09 +0200

    project updated

--- components/Category/Products/index.tsx
+++ components/Category/Products/index.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useCallback, useEffect, useState, useRef} from 'react';
+import React, { FC, useCallback, useEffect, useState, useRef } from 'react';
 import { Photo, PhotosWithTotalResults } from 'pexels';
 // types
 import { Categories, SearchQuery } from '../../../lib/Types';
@@ -12,85 +12,83 @@ import styles from '../../../styles/css/category_products.module.css';
 
 
 
-const Products:FC<{categoryName: Categories}> = ({categoryName: name}) => {
+const Products: FC<{ categoryName: Categories }> = ({ categoryName: name }) => {
   const [query, setQuery] = useState<SearchQuery>();
   const [photos, setPhotos] = useState<Photo[]>();
   const [pageNum, setPageNum] = useState(0);
   const loader = useRef(null);
-  
+
   const fetchData = async () => {
-    if(!query) return;
+    if (!query) return;
     const res = await fetch(`https://api.pexels.com/v1/search/?page=${pageNum}&per_page=${per_page}&query=${query}`, {
-    method: 'GET',
-    headers: {
-     'content-type': 'application/json',
-    'Authorization': '563492ad6f9170000100000130ea875f314545468bc8864c0008d7a5'
-    }});
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': '563492ad6f9170000100000130ea875f314545468bc8864c0008d7a5'
+      }
+    });
     const data = await res.json() as PhotosWithTotalResults;
     setPhotos(prev => {
-      if(prev){
+      if (prev) {
         return [...prev, ...data.photos] as Photo[];
-      }else{
+      } else {
         return data.photos;
       }
     });
   };
-  const handleObserver = useCallback((entries:IntersectionObserverEntry[]) => {
+  const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
     const target = entries[0];
-    if(target.isIntersecting) {
+    if (target.isIntersecting) {
       setPageNum(prev => prev + 1);
     }
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     const observer = new IntersectionObserver(handleObserver, intersectionOption);
-    if(loader.current) observer.observe(loader.current);
+    if (loader.current) observer.observe(loader.current);
   }, [handleObserver]);
 
   // set query
-  useEffect(()=>{
-    if(!name) return;
-    switch(name){
-     case 'paintings':
-      setQuery('abstract');
-      break;
-     case 'drawings':
-      setQuery('illustration');
-      break;
-     case 'sculpture':
-      setQuery('sculpture');
-      break;
-     default:
-      setQuery('');
-      break;
+  useEffect(() => {
+    if (!name) return;
+    switch (name) {
+      case 'paintings':
+        setQuery('abstract');
+        break;
+      case 'drawings':
+        setQuery('illustration');
+        break;
+      case 'sculpture':
+        setQuery('sculpture');
+        break;
+      default:
+        setQuery('');
+        break;
     }
   }, [name])
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
   }, [pageNum]);
-  
-  useEffect(()=>{
-    setPageNum(0);
+
+  useEffect(() => {
+    if(!query) return;
     setPhotos([]);
-  },[query]);
- return (
-  <>
-    <div className={styles.products_container}>
-      {photos?.map((photo, index) => {
-        const {src: {original: imageSrc}, photographer: author, id} = photo;
-        if(index === per_page - 1){
-          <Product imageSrc={imageSrc} name={'Wallowing Breeze'} author={author} id={id} key={id}/>
-        }else{
-          return(
-          <Product imageSrc={imageSrc} name={'Wallowing Breeze'} author={author} id={id} key={id}/>
-        );
-        }
-      })}
-    </div>
-    <div ref={loader}></div>
-  </>
- );
+  }, [query]);
+
+  return (
+    <>
+      <div className={styles.products_container}>
+        {photos?.map(photo => {
+          const { src: { original: imageSrc }, photographer: author, id } = photo;
+            return (
+              <Product imageSrc={imageSrc} name={'Wallowing Breeze'} author={author} id={id} key={id} />
+            );
+        })}
+      </div>
+      <div ref={loader}></div>
+    </>
+  );
 };
- 
+
 export default Products;
