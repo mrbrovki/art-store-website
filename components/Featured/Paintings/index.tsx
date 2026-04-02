5af90f35ccff09b515bdf2858d23a151a02dc002
Mahkamjon Raupov <devbrovki@gmail.com>
2026-04-02 21:12:18 +0200

    bug fixes

--- components/Featured/Paintings/index.tsx
+++ components/Featured/Paintings/index.tsx
@@ -1,37 +1,43 @@
-import React, {FC} from 'react';
+import React, { FC } from "react";
 // types
-import { Media } from '../../../lib/Types';
+import { Media } from "../../../lib/Types";
 // components
-import Painting from './Painting';
+import Painting from "./Painting";
 // styles
-import styles from '../../../styles/css/featured_paintings.module.css';
+import styles from "../../../styles/css/featured_paintings.module.css";
 
-
-
-const Paintings:FC<{media: Media}> = ({media}) => {
- return (
-   <>
-    <h1 className={styles.heading}>featured paintings</h1>
-    <section className={styles.paintings_container}>
-      <div className={styles.carousel}>
-        <div className={styles.wrapper_1}>
-          {media.slice(0, 4).map(painting => {
-            return(
-              <Painting key={painting.id} src={painting.src.original} id={painting.id}/>
-            )
-          })}
+const Paintings: FC<{ media: Media }> = ({ media }) => {
+  return (
+    <>
+      <h1 className={styles.heading}>featured paintings</h1>
+      <section className={styles.paintings_container}>
+        <div className={styles.carousel}>
+          <div className={styles.wrapper_1}>
+            {media.slice(0, 7).map((painting) => {
+              return (
+                <Painting
+                  key={painting.id}
+                  src={painting.src.original}
+                  id={painting.id}
+                />
+              );
+            })}
+          </div>
+          <div className={styles.wrapper_2}>
+            {media.slice(7, 14).map((painting) => {
+              return (
+                <Painting
+                  key={painting.id}
+                  src={painting.src.original}
+                  id={painting.id}
+                />
+              );
+            })}
+          </div>
         </div>
-        <div className={styles.wrapper_2}>
-          {media.slice(4, 8).map(painting => {
-            return(
-              <Painting key={painting.id} src={painting.src.original} id={painting.id}/>
-            )
-          })}
-        </div>
-      </div>
-    </section>
-   </>
- );
+      </section>
+    </>
+  );
 };
- 
+
 export default Paintings;
